5d0dd8da3ecd147ea1ad5021ae3c82de0eb7cdce
koga.shintaro <koga.shintaro@jobirun.com>
2025-04-04 10:35:37 +0000

    update contact page

--- app/(marketing)/contact/page.tsx
+++ app/(marketing)/contact/page.tsx
@@ -72,6 +72,27 @@ export default function ContactPage() {
                   </p>
                   <dl className="mt-8 space-y-6">
                     <dt>
+                      <span className="sr-only">会社名</span>
+                    </dt>
+                    <dd className="flex text-base text-blue-50">
+                      <svg
+                        className="flex-shrink-0 h-6 w-6 text-blue-200"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
+                        />
+                      </svg>
+                      <span className="ml-3">合同会社JOBIRUN</span>
+                    </dd>
+                    <dt>
                       <span className="sr-only">住所</span>
                     </dt>
                     <dd className="flex text-base text-blue-50">
@@ -97,13 +118,13 @@ export default function ContactPage() {
                         />
                       </svg>
                       <span className="ml-3">
-                        〒000-0000
+                        〒010-0923
                         <br />
-                        ○○県○○市○○町0-0
+                        秋田県秋田市保戸野金砂町４ー３１
                       </span>
                     </dd>
                     <dt>
-                      <span className="sr-only">電話番号</span>
+                      <span className="sr-only">ウェブサイト</span>
                     </dt>
                     <dd className="flex text-base text-blue-50">
                       <svg
@@ -118,31 +139,14 @@ export default function ContactPage() {
                           strokeLinecap="round"
                           strokeLinejoin="round"
                           strokeWidth="2"
-                          d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                          d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
                         />
                       </svg>
-                      <span className="ml-3">TEL: 00-0000-0000</span>
-                    </dd>
-                    <dt>
-                      <span className="sr-only">メールアドレス</span>
-                    </dt>
-                    <dd className="flex text-base text-blue-50">
-                      <svg
-                        className="flex-shrink-0 h-6 w-6 text-blue-200"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                        />
-                      </svg>
-                      <span className="ml-3">Email: info@example.com</span>
+                      <span className="ml-3">
+                        <a href="https://jobirun.com" className="text-blue-100 hover:text-white">
+                          https://jobirun.com
+                        </a>
+                      </span>
                     </dd>
                   </dl>
                 </div>
@@ -154,9 +158,9 @@ export default function ContactPage() {
                 
                 <div className="w-full">
                   <iframe 
-                    src="https://docs.google.com/forms/d/e/1Qk7MOkGCiPKIw4ZjLZpXmVWh9pgu1g4ztlswNFa5d8k/viewform?embedded=true" 
+                    src="https://docs.google.com/forms/d/e/1FAIpQLScdW6g5EdL4tn25yYbCt-I7dCaKjD4rxskbGKhBh-ZSX3C0Jw/viewform?embedded=true" 
                     width="100%" 
-                    height="800" 
+                    height="1000" 
                     frameBorder="0" 
                     marginHeight={0} 
                     marginWidth={0}
